93fcdb5189cef489f9dfee88432003e4c10470aa
liguanrui <xhsysu@126.com>
2015-03-08 23:55:43 +0800

    【开发】更新基本例子

--- base/js/main.js
+++ base/js/main.js
@@ -1,35 +1,24 @@
-var app = angular.module('myApp1', ['ui.mc.grid', 'ui.bootstrap']);
-app.controller('MyCtrl1',
-function($scope) {
-    $scope.dataSource = [{
-        name: "Moroni",
-        age: 50
-    },
-    {
-        name: "Tiancum",
-        age: 43
-    },
-    {
-        name: "Jacob",
-        age: 27
-    },
-    {
-        name: "Nephi",
-        age: 29
-    },
-    {
-        name: "Enos",
-        age: 34
-    }];
-    $scope.gridOptions ={
-	columnDefs:[{
-        key: "name",
-        name: "姓名",
-        type: "text"
-    },
-    {
-        key: "age",
-        name: "年龄",
-        type: "text"
-    }]};
+var app = angular.module('myApp', ['ui.mc.grid', 'ui.bootstrap']);
+app.controller('MyCtrl1',function($scope) {
+    $scope.dataSource = [{name: "Moroni",age: 50},
+                         {name: "Tiancum",age: 43},
+                         {name: "Jacob",age: 27},
+                         {name: "Nephi",age: 29},
+                         {name: "Enos",age: 34}];
+    $scope.gridOptions ={columnDefs:[{key: "name",name: "姓名",type: "text"},
+                                     {key: "age",name: "年龄",type: "text"}]
+                        };
+});
+
+
+//-------------------------------
+app.controller('MyCtrl2',function($scope) {
+    $scope.dataSource = [{name: "Moroni",age: 50},
+                         {name: "Tiancum",age: 43},
+                         {name: "Jacob",age: 27},
+                         {name: "Nephi",age: 29},
+                         {name: "Enos",age: 34}];
+    $scope.gridOptions ={columnDefs:[{key: "name",name: "姓名",type: "text"},
+                                     {key: "age",name: "年龄",type: "text"}]
+                        };
 });
